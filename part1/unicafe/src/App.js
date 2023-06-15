7acbd64f5f5a908ed7ef4fca5858bb9c56ea1e49
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-15 15:22:32 +0100

    Ability to increase the states for each option

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,41 @@
+import { useState } from "react";
+
+function FeedbackButton({onClick, text}) {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
 function App() {
+  // states
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => {
+    setGood(good + 1);
+  }
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const increaseBad = () => {
+    setBad(bad + 1);
+  }
+
   return (
     <div className="App">
+      <h1>Give feedback</h1>
+
+      <FeedbackButton onClick={increaseGood} text={"Good"} />
+      <FeedbackButton onClick={increaseNeutral} text={"Neutral"} />
+      <FeedbackButton onClick={increaseBad} text={"Bad"} />
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   );
 }
