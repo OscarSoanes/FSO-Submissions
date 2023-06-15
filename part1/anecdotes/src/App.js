c2b89ac5a78ca23054860a78a0214fecd0cedeb3
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-15 16:38:55 +0100

    Added an ability to display the anecdote with the most votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0);
+  const [highestVoteId, setHighestVoteId] = useState(0);
 
   const increaseSelected = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
@@ -22,15 +23,41 @@ function App() {
   const increaseVoteAtSelected = () => {
     const newPoints = [...points];
     newPoints[selected] += 1
-    setPoints(newPoints);
+     setPoints(newPoints);
+
+    topAnecdote(newPoints);
+  }
+
+  const topAnecdote = (points) => {
+    const top = {
+      votes: -1,
+      index: -1,
+    };
+
+    points.forEach((vote, index) => {
+      if (top.votes < vote) {
+        top.votes = vote;
+        top.index = index;
+      }
+    })
+    
+    if (topAnecdote.index !== highestVoteId) {
+      setHighestVoteId(top.index);
+    }
   }
 
   return (
     <div className="App">
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>{points[selected]} votes</p>
+
       <button onClick={increaseVoteAtSelected}>Vote</button>
       <button onClick={increaseSelected}>Next anecdote</button>
+
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[highestVoteId]}</p>
+      <p>{points[highestVoteId]} votes</p>
     </div>
   );
 }
