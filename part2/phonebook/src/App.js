aabd194749c6da3e872298a2092cf4a5d030e66d
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-15 22:09:12 +0100

    Added starter code. Added an ability to set and see names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,45 @@
-function App() {
-  return (
-    <div className="App">
+import { useState } from 'react'
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { id: 0 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const submitName = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+      id: persons.length,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitName}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.id}>{person.name}</p>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
