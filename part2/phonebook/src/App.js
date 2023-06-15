056d151a7105aee2cb29da798a344e9a2c042a64
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-15 22:24:11 +0100

    Added an ability filter based on name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0, number: "07123456789" },
+    { name: 'Arto Hellas', number: '040-123456', id: 0 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const peopleToShow = persons.filter(person => 
+    person.name.toLowerCase()
+    .includes(filter.toLowerCase()
+  ));
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -51,7 +60,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {peopleToShow.map((person) => (
         <p key={person.id}>{person.name} {person.number}</p>
       ))}
     </div>
