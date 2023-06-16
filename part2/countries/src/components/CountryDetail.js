433946157e8a2f3eb241927d64dbf4adeb942b3e
Oscar Soanes <oscarsoanes@gmail.com>
2023-06-16 15:46:25 +0100

    Added an ability to see the weather alongside the country info

--- part2/countries/src/components/CountryDetail.js
+++ part2/countries/src/components/CountryDetail.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import weatherService from '../services/weather';
 
 export default function CountryDetail({country}) {
-  console.log(country);
-  const languages = () => {
+  const [languages, setLanguages] = useState([]);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    console.log("Setting Weather");
+    weatherService
+      .getWeatherAtLocation(country.capital[0])
+      .then(countriesWeather => {
+        setWeather(countriesWeather);
+        console.log(countriesWeather);
+      })
+  }, [country.capital])
+
+  useEffect(() => {
+    console.log("Updating Languages")
+
     const allLanguages = [];
     for (const language in country.languages) {
       allLanguages.push(country.languages[language]);
     }
+    setLanguages(allLanguages);
+  }, [country.languages])
 
-    return allLanguages;
-  }
-  
   return (
     <>
       <h1>{country.name.common}</h1>
@@ -19,12 +34,22 @@ export default function CountryDetail({country}) {
 
       <h2>Languages:</h2>
       <ul>
-        {languages().map(language => (
+        {languages.map(language => (
           <li key={language}>{language}</li>
         ))}
       </ul>
 
       <img src={`${country.flags.png}`} alt={`${country.name.common}'s flag`}/>
+      
+      {weather !== null && (
+        <div>
+          <h2>Weather: </h2>
+          <p>Condition: {weather.weather[0].main}</p>
+          <p>Temperature: {(weather.main.temp - 273.15).toFixed(1)}C</p>
+  
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt=""/>
+        </div>
+      )}
     </>
   )
 }
